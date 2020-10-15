2fb26248f158d2d7f71cfbf9b2c8d5ca75f39e41
wupojung <wupojung@gmail.com>
2020-10-15 20:31:39 +0800

    加入專案檔案。

---
+++ MsgDemo/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace MsgDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGo = new System.Windows.Forms.Button();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnGo.Location = new System.Drawing.Point(272, 275);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(230, 72);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // tbInput
+            // 
+            this.tbInput.Font = new System.Drawing.Font("新細明體", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tbInput.Location = new System.Drawing.Point(163, 87);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(441, 84);
+            this.tbInput.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.btnGo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox tbInput;
+    }
+}
+
---
+++ MsgDemo/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MsgDemo
+{
+    public partial class Form1 : Form
+    {
+
+        int num = 55;
+        public Form1()
+        {
+            InitializeComponent();
+
+            //TODO: 
+            num = new Random().Next(100);
+
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //提取輸入內容
+                string input_str = tbInput.Text;
+                //轉換int 
+                int input_int = int.Parse(input_str);
+
+                //TODO: 寫判斷邏輯
+
+                //顯示內容
+                MessageBox.Show($"{input_int}", "Random Num", MessageBoxButtons.OK);
+            }
+            catch (Exception)
+            { 
+                //錯誤提示
+                MessageBox.Show($"不要亂輸入", "警告", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //throw;
+            }
+        }
+    }
+}
